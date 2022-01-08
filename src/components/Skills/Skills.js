1ecb690a8c232a999c51e34359656e448145e6d1
divyxnsh <divyxnsh@gmail.com>
2022-01-07 21:18:48 -0500

    Updated Skills.js

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -8,6 +8,11 @@ import {
   DiHtml5,
   DiCss3,
   DiMongodb,
+  DiGit,
+  DiLinux,
+  DiAws,
+  DiDocker,
+  DiHeroku,
 } from "react-icons/di";
 import {
   Section,
@@ -29,10 +34,12 @@ const Skills = () => (
     <List>
       <ListItem>
         <picture>
-          <DiJavascript1 size="4rem" />
-          <DiPython size="4rem" />
-          <SiLua size="4rem" />
-          <SiCplusplus size="4rem" />
+          <DiJavascript1 size="2rem" />
+          <DiPython size="2rem" />
+          <SiLua size="2rem" />
+          <SiCplusplus size="2rem" />
+          <DiHtml5 size="2rem" />
+          <DiCss3 size="2rem" />
         </picture>
         <ListContainer>
           <ListTitle>Languages</ListTitle>
@@ -43,41 +50,54 @@ const Skills = () => (
               <li>&#8226; Javascript</li>
               <li>&#8226; MATLAB</li>
               <li>&#8226; Lua</li>
+              <li>&#8226; HTML5</li>
+              <li>&#8226; CSS3</li>
             </ul>
           </ListParagraph>
         </ListContainer>
       </ListItem>
       <ListItem>
         <picture>
-          <DiReact size="4rem" />
-          <DiHtml5 size="4rem" />
-          <DiCss3 size="4rem" />
+          <DiReact size="3rem" />
+          <DiMysql size="3rem" />
+          <DiMongodb size="3rem" />
+          <DiGit size="3rem" />
         </picture>
         <ListContainer>
           <ListTitle>Technologies</ListTitle>
           <ListParagraph>
             <ul>
               <li>&#8226; React.js</li>
-              <li>&#8226; HTML5</li>
-              <li>&#8226; CSS3</li>
-              <li>&#8226; Linux</li>
-              <li>&#8226; MS Azure</li>
+              <li>&#8226; MySQL</li>
+              <li>&#8226; MongoDB</li>
               <li>&#8226; Git</li>
+              <li>&#8226; Discord.py</li>
+              <li>&#8226; BeautifulSoup 4</li>
+              <li>&#8226; mysql-connector</li>
+              <li>&#8226; PyMongo/Motor</li>
+              <li>&#8226; Pillow</li>
             </ul>
           </ListParagraph>
         </ListContainer>
       </ListItem>
       <ListItem>
         <picture>
-          <DiMysql size="4rem" />
-          <DiMongodb size="4rem" />
+          <DiLinux size="3rem" />
+          <DiAws size="3rem" />
+          <DiDocker size="3rem" />
+          <DiHeroku size="3rem" />
         </picture>
         <ListContainer>
-          <ListTitle>Databases</ListTitle>
+          <ListTitle>Tools</ListTitle>
           <ListParagraph>
             <ul>
-              <li>&#8226; MySQL</li>
-              <li>&#8226; MongoDB</li>
+              <li>&#8226; Linux</li>
+              <li>&#8226; Docker</li>
+              <li>&#8226; Screen</li>
+              <li>&#8226; AWS</li>
+              <li>&#8226; MS Azure</li>
+              <li>&#8226; Heroku</li>
+              <li>&#8226; MS Office</li>
             </ul>
           </ListParagraph>
         </ListContainer>
